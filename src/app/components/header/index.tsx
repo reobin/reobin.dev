5834b16acf17c0121a7499cd50b8d67173fea521
Robin Gagnon <me@reobin.dev>
2023-11-18 13:02:38 -0500

    feat: Add logo to header and favicon

--- src/app/components/header/index.tsx
+++ src/app/components/header/index.tsx
@@ -1,8 +1,10 @@
+import Image from 'next/image';
 import styles from './header.module.css';
 
 function Header() {
   return (
     <header className={styles.header}>
+      <Image src="/logo.png" alt="reobin.dev logo" width={50} height={50} />
       <h2>reobin.dev</h2>
     </header>
   );
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,9 @@
+import styles from './page.module.css';
+
 export default function Home() {
-  return "Hello, I'm Robin.";
+  return (
+    <div className={styles.home}>
+      Hello, I&apos;m Robin, a web developer based in Montreal, QC.
+    </div>
+  );
 }
