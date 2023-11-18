c039748c3b4cf1ac740a79a124b0e97eb6df62fc
Robin Gagnon <me@reobin.dev>
2023-11-18 17:21:46 -0500

    feat: Remove current year in footer

--- src/app/components/footer/index.tsx
+++ src/app/components/footer/index.tsx
@@ -1,16 +1,14 @@
 import styles from './footer.module.css';
 
 function Footer() {
-  const year = new Date().getFullYear();
   return (
     <footer className={styles.footer}>
       <div className={styles.footerContent}>
         <a href="https://github.com/reobin" target="_blank">
-          Follow on Github
+          Follow my work on Github
         </a>
         <span>
-          © {year}, Contact me at{' '}
-          <a href="mailto:me@reobin.dev">me@reobin.dev</a>
+          Contact me at <a href="mailto:me@reobin.dev">me@reobin.dev</a>
         </span>
       </div>
     </footer>
