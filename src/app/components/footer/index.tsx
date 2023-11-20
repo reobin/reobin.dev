55815f8aed05430d4d82b3b06cf15e76550612bb
Robin Gagnon <me@reobin.dev>
2023-11-20 11:48:26 -0500

    feat: Add github icon next to link

--- src/app/components/footer/index.tsx
+++ src/app/components/footer/index.tsx
@@ -1,3 +1,5 @@
+import IconGithub from '../icons/github';
+
 import styles from './footer.module.css';
 
 function Footer() {
@@ -5,7 +7,7 @@ function Footer() {
     <footer className={styles.footer}>
       <div className={styles.footerContent}>
         <a href="https://github.com/reobin" target="_blank">
-          Follow my work on Github
+          Follow my work on Github <IconGithub />
         </a>
         <span>
           Contact me at <a href="mailto:me@reobin.dev">me@reobin.dev</a>
