671b00681e9bc6394df56b25bf1ba9f8439d33f3
Robin Gagnon <me@reobin.dev>
2023-11-26 14:42:40 -0500

    feat: Add color transition to masonry cards

--- src/app/masonry/page.tsx
+++ src/app/masonry/page.tsx
@@ -1,3 +1,6 @@
+'use client';
+
+import { useEffect, useState } from 'react';
 import cn from 'classnames';
 
 import styles from './masonry.module.css';
@@ -15,12 +18,28 @@ const COLORS = [
 ];
 
 function Masonry() {
+  const [indexOffset, setIndexOffset] = useState<number>(0);
+
+  useEffect(() => {
+    setIndexOffset(indexOffset => indexOffset + 1);
+  }, []);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setIndexOffset(indexOffset => indexOffset + 1);
+    }, 2500);
+    return () => clearInterval(interval);
+  }, []);
+
   return (
     <section className={styles.container}>
       {Array.from(Array(9)).map((_, index) => (
         <article
           key={index}
-          className={cn(styles.article, styles[COLORS[index]])}
+          className={cn(
+            styles.article,
+            styles[COLORS[(index + indexOffset) % 9]],
+          )}
         />
       ))}
     </section>
